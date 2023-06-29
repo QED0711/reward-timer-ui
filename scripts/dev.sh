f6c1da05c15098a3cc1acc3d8bd4a4120ea39c07
QED0711 <qdizon@gmail.com>
2023-06-29 06:52:58 -0400

    updated server to serve static built files

---
+++ scripts/dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+npm start &
+cd server
+npm start &
--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 // import db from './db/db.js'
 import routes from './routes.js'
 
 const app = express();
 const port = 8000
 
-app.get("/", (req, res) => {
-    res.send("Hello world!!!");
-})
+app.use("/", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "..", "dist")))
+
+// app.get("/", (req, res) => {
+//     res.send("Hello world!!!");
+// })
 
 for(let route of routes) {
     app[route.method]?.(route.path, bodyParser.json(), route.callback)
