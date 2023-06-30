faf91a35fc17cafb2d9e5e7bd0f80c28e96c23a1
QED0711 <qdizon@gmail.com>
2023-06-29 21:11:30 -0400

    added new user forms

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -6,9 +6,11 @@ import mainManager from '../../state/main/mainManager';
 
 // =============================== COMPONENTS =============================== 
 import UserList from '../users/UserList'
+import UserForms from '../users/NewUserForms';
 
 // =============================== ICONS =============================== 
 import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
+import Modal from './Modal';
 
 
 // EVENTS
@@ -43,6 +45,8 @@ export default function Sidebar({ }) {
     const [searchFilter, setSearchFilter] = useState("");
     const [listHeight, setListHeight] = useState("100vh")
     const [sidebarTranslation, setSidebarTranslation] = useState(0);
+    const [showNewUserModal, setShowNewUserModal] = useState(false);
+    const [showNewAdminModal, setShowNewAdminModal] = useState(false);
     const sidebarRef = useRef(null)
     const listContainerRef = useRef(null)
 
@@ -51,29 +55,43 @@ export default function Sidebar({ }) {
     toggleCollapse(state.showSidebar, setSidebarTranslation, sidebarRef)
 
     return (
-        <div
-            ref={sidebarRef}
-            className='absolute top-0 left-0 w-screen lg:w-[33vw] h-screen py-2 pl-2 bg-indigo-200 shadow-md shadow-gray-800'
-            style={{ transform: `translateX(${sidebarTranslation}px)` }}
-        >
-            <div className='absolute w-8 h-8 px-2 right-[-32px] top-4 bg-indigo-200 cursor-pointer' onClick={toggleSidebarClick}>
-                {
-                    state.showSidebar
-                        ? <MdArrowBackIos className='h-full w-full' size={"2rem"} />
-                        : <MdArrowForwardIos className='h-full w-full' size={"2rem"} />
-                }
+        <>
+            {showNewUserModal && <Modal closeButton onClose={() => setShowNewUserModal(false)}><UserForms.NewUser onClose={() => {setShowNewUserModal(false)}} /></Modal>}
+            {showNewAdminModal && <Modal closeButton onClose={() => setShowNewAdminModal(false)}><UserForms.NewAdmin onClose={() => setShowNewAdminModal(false)} /></Modal>}
+            <div
+                ref={sidebarRef}
+                className='absolute top-0 left-0 w-screen lg:w-[33vw] h-screen py-2 pl-2 bg-indigo-200 shadow-md shadow-gray-800'
+                style={{ transform: `translateX(${sidebarTranslation}px)` }}
+            >
+                <div className='absolute w-8 h-8 px-2 right-[-32px] top-4 bg-indigo-200 cursor-pointer' onClick={toggleSidebarClick}>
+                    {
+                        state.showSidebar
+                            ? <MdArrowBackIos className='h-full w-full' size={"2rem"} />
+                            : <MdArrowForwardIos className='h-full w-full' size={"2rem"} />
+                    }
+                </div>
+                <div className='w-full pr-2'>
+                    <div className='grid grid-cols-2 gap-2 mb-2'>
+                        <button 
+                            className='bg-indigo-100 hover:shadow-sm hover:shadow-indigo-500 text-indigo-800'
+                            onClick={() => setShowNewUserModal(true)}
+                        >New User</button>
+                        <button 
+                            className='bg-indigo-100 hover:shadow-sm hover:shadow-indigo-500 text-indigo-800'
+                            onClick={() => setShowNewAdminModal(true)}
+                        >New Admin</button>
+                    </div>
+                    <input
+                        type="search"
+                        placeholder='Search for User'
+                        className='w-full p-1 mb-2 text-lg bg-indigo-50 text-indigo-800 rounded-md '
+                        onChange={e => setSearchFilter(e.target.value)}
+                    />
+                </div>
+                <div ref={listContainerRef} className='pr-2 overflow-auto' style={{ height: listHeight }}>
+                    <UserList {...{ searchFilter }} />
+                </div>
             </div>
-            <div className='w-full pr-2'>
-                <input
-                    type="search"
-                    placeholder='Search for User'
-                    className='w-full p-1 mb-2 text-lg bg-indigo-50 text-indigo-800 rounded-md '
-                    onChange={e => setSearchFilter(e.target.value)}
-                />
-            </div>
-            <div ref={listContainerRef} className='pr-2 overflow-auto' style={{ height: listHeight }}>
-                <UserList {...{ searchFilter }} />
-            </div>
-        </div>
+        </>
     )
 }
---
+++ src/components/users/NewUserForms.jsx
@@ -0,0 +1,71 @@
+// STYLES
+const INPUT_STYLE = "form-input inline-block w-full m-2 p-1 rounded-sm bg-white text-gray-800 shadow-sm shadow-gray-800 resize-none"
+
+// COMPONENTS
+const FormLabel = ({ text, display = "block", className, children }) => {
+    return (
+        <label className={`${display} mx-1 ${className}`}>
+            <sub className={`block relative top-2 mx-2 text-sm`}>{text}</sub>
+            {children}
+        </label>
+    )
+}
+
+
+export default {
+    NewUser({ onClose }) {
+
+        // EVENTS
+        const handleSubmit = e => {
+            e.preventDefault();
+            const user = {
+                name: document.querySelector(".form-input").value,
+                points: 0,
+                tasks: [],
+                timers: [],
+                rewards: [],
+                deductions: []
+            }
+
+            console.log(user)
+            onClose();
+        }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <h2 className="text-center text-2xl font-bold capitalize">Create New User</h2>
+                <FormLabel text="Name">
+                    <input className={INPUT_STYLE} type="text" autoFocus />
+                </FormLabel>
+                <input className="block w-full mx-auto mt-4 bg-yellow-300 text-indigo-700 shadow-sm shadow-gray-800 cursor-pointer" type="submit" value="Create" />
+            </form>
+        )
+    },
+
+    NewAdmin({onClose}) {
+        // EVENTS
+        const handleSubmit = e => {
+            e.preventDefault();
+            const inputs = document.querySelectorAll(".form-input") 
+            const admin = {}
+            for(let input of inputs) {
+                admin[input.dataset.field] = input.value;
+            }
+            console.log(admin);
+            onClose();
+        }
+        
+        return (
+            <form onSubmit={handleSubmit}>
+                <h2 className="text-center text-2xl font-bold capitalize">Create New Admin</h2>
+                <FormLabel text="Name">
+                    <input className={INPUT_STYLE} type="text" data-field="name" autoFocus />
+                </FormLabel>
+                <FormLabel text="Passcode">
+                    <input className={INPUT_STYLE} type="number" data-field="unlockCode" />
+                </FormLabel>
+                <input className="block w-full mx-auto mt-4 bg-yellow-300 text-indigo-700 shadow-sm shadow-gray-800 cursor-pointer" type="submit" value="Create" />
+            </form>
+        )
+    },
+}
