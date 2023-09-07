b005fd8baa345f1e0bbe944fc555e1fc0f7b2e8d
QED0711 <qdizon@gmail.com>
2023-09-07 05:11:58 -0400

    updated server to run on just http and handle https through proxy

--- scripts/create_ssl_keys.sh
+++ scripts/create_ssl_keys.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-openssl req -nodes -new -x509 -keyout ./ssl/server.key -out ./ssl/server.cert
+if [ -z "$1" ]; then
+  echo "Usage: $0 <IP_ADDRESS>"
+  exit 1
+fi
+
+IP=$1
+
+openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
+  -extensions SAN \
+  -config <(cat /etc/ssl/openssl.cnf \
+  <(printf "\n[SAN]\nsubjectAltName=IP:$IP")) \
+  -keyout ssl/server.key \
+  -out ssl/server.cert
+
+
+# openssl req -nodes -new -x509 -keyout ./ssl/server.key -out ./ssl/server.cert -days 365
 # openssl req -x509 -newkey rsa:4096 -keyout ./ssl/key.pem -out ./ssl/cert.pem -days 365
--- server/index.js
+++ server/index.js
@@ -7,15 +7,10 @@ import { fileURLToPath } from 'node:url';
 import cors from 'cors';
 import { Server } from "socket.io";
 import http from "node:http";
-import https from 'node:https';
 import routes from './routes.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const httpServer = express();
-httpServer.all("*", (req, res) => {
-    res.redirect(301, `https://${req.headers.host}${req.url}`);
-})
 const app = express();
 
 // CORS
@@ -41,12 +36,7 @@ if(process.env.BASIC_AUTH === "true" && auth) {
 // SERVE STATIC
 app.use("/", express.static(path.join(__dirname, "..", "dist")))
 
-// ENABLE SSL
-const privateKey = fs.readFileSync(path.join(__dirname, "..", "ssl", "server.key"), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, "..", "ssl", "server.cert"), 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const server = https.createServer(credentials, app);
+const server = http.createServer(app);
 export const io = new Server(server, { cors: corsOptions });
 
 // API Key Verification
@@ -54,7 +44,7 @@ app.use(function(req, res, next){
     const apiKey = req.headers["x-api-key"];
     if("apiKeys" in auth && (!apiKey || !auth.apiKeys?.includes?.(apiKey))){
         res.status(401)
-        return res.json({"apiKeyError": "API Key missing or no found match"})
+        return res.json({"apiKeyError": "API Key missing or no match found"})
     }
     next();
 })
@@ -64,13 +54,10 @@ for (let route of routes) {
 
 io.on('connection', socket => {
     console.log("=== NEW CONNECTION ===");
-    // io.to(socket.id).emit("serverTime", {time: Date.now()})
     io.emit("serverTime", { time: Date.now(), timezone: Intl.DateTimeFormat().resolvedOptions().timeZone });
 })
 
 // START SERVER(S)
 const HTTP_PORT = 8000
-const HTTPS_PORT = 8443
 
-httpServer.listen(HTTP_PORT, () => console.log(`Redirecting from http://localhost:${HTTP_PORT}`))
-server.listen(HTTPS_PORT, () => console.log(`Serving at https://localhost:${HTTPS_PORT}/`))
+server.listen(HTTP_PORT, () => console.log(`Serving at http://localhost:${HTTP_PORT}/`))
