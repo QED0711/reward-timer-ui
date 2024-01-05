d7cc466eaaa8b9323ff6324826241e0d58ad5b85
QED0711 <qdizon@gmail.com>
2024-01-05 06:03:47 -0500

    updated port forwarding

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,7 @@
 
 docker run --rm -ti -d \
     --name reward_timer \
-    -p 80:8000 \
-    -p 443:8443 \
+    -p 8000:8000 \
     -v $PWD/ssl/:/app/ssl \
     -v $PWD/server/auth.json:/app/server/auth.json \
     -v $PWD/server/db/:/app/server/db:z \
